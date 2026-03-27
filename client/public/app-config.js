b00ed9a98a27a6094ed87bc5b89454db85d00564
MazieroMarco <maziero.marco@gmail.com>
2026-03-27 12:23:27 +0100

    Added prod backedn link

--- client/public/app-config.js
+++ client/public/app-config.js
@@ -1,5 +1,5 @@
 window.__REFLOYD_CONFIG__ = {
     // Leave empty to use the same origin as the frontend.
     // Example: 'https://api.example.com'
-    backendUrl: '',
+    backendUrl: 'https://re-floyd-backend.onrender.com',
 };
